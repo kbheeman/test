ecd1acd98482761f6f12d779315d596e33b6e84b
kbheeman <krishnamurthy.rao@thehartford.com>
2017-05-22 21:39:42 -0400

    test

---
+++ HelloAngular/WebRoot/HelloWorldApp/app.js
@@ -0,0 +1,2 @@
+ "use strict";
+ var helloApp = angular.module("HelloWordApp", []);
---
+++ HelloAngular/WebRoot/HelloWorldApp/controller/helloctrl.js
@@ -0,0 +1,4 @@
+ "use strict";
+angular.module("HelloWordApp", []).controller("HelloCtrl", function($scope) {
+	$scope.name = "LLLL";
+});;
